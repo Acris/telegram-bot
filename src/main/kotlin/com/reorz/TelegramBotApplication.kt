7b2c3ce35500e406fdda8c432c3986acbb3e9b20
Acris Liu <acrisliu@gmail.com>
2016-08-13 22:29:29 +0800

    register bot

--- src/main/kotlin/com/reorz/TelegramBotApplication.kt
+++ src/main/kotlin/com/reorz/TelegramBotApplication.kt
@@ -1,13 +1,43 @@
 package com.reorz
 
+import com.reorz.handler.MessageHandler
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.telegram.telegrambots.TelegramApiException
+import org.telegram.telegrambots.TelegramBotsApi
+import org.telegram.telegrambots.logging.BotLogger
+import org.telegram.telegrambots.updatesreceivers.BotSession
+import javax.annotation.PostConstruct
+import javax.annotation.PreDestroy
 
 @SpringBootApplication
 open class TelegramBotApplication {
+    @Autowired lateinit var messageHandler: MessageHandler
+    var botSession: BotSession? = null
+
     companion object {
+        val LOGTAG: String = TelegramBotApplication::class.java.name
+
         @JvmStatic fun main(args: Array<String>) {
             SpringApplication.run(TelegramBotApplication::class.java, *args)
         }
+    }
+
+    @PostConstruct
+    fun start() {
+        val telegramBotsApi = TelegramBotsApi()
+        try {
+            botSession = telegramBotsApi.registerBot(messageHandler)
+            BotLogger.info(LOGTAG, "Telegram bot ${messageHandler.botUsername} started")
+        } catch (e: TelegramApiException) {
+            BotLogger.error(LOGTAG, "Start bot error", e)
+        }
+    }
+
+    @PreDestroy
+    fun stop() {
+        botSession?.close()
+        BotLogger.info(LOGTAG, "Telegram bot ${messageHandler.botUsername} stopped")
     }
 }
