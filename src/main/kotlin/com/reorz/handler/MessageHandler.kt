8c7ef6536c4d3f9866d49002ae33dfbf93d7a678
Acris Liu <acrisliu@gmail.com>
2016-08-13 22:28:33 +0800

    add message handler

--- src/main/kotlin/com/reorz/handler/MessageHandler.kt
+++ src/main/kotlin/com/reorz/handler/MessageHandler.kt
@@ -3,6 +3,8 @@ package com.reorz.handler
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
+import org.telegram.telegrambots.TelegramApiException
+import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Update
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 
@@ -10,7 +12,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot
  * Created by acris on 2016/8/13.
  */
 @Component
-class DirectionsHandler : TelegramLongPollingBot() {
+class MessageHandler : TelegramLongPollingBot() {
     @Autowired lateinit var environment: Environment
 
     override fun getBotUsername(): String {
@@ -22,6 +24,18 @@ class DirectionsHandler : TelegramLongPollingBot() {
     }
 
     override fun onUpdateReceived(update: Update?) {
+        if (update!!.hasMessage()) {
+            val message = update.message
+            if (message.hasText()) {
+                val sm = SendMessage()
+                sm.chatId = message.chatId.toString()
+                sm.text = message.text
+                try {
+                    sendMessage(sm)
+                } catch (e: TelegramApiException) {
 
+                }
+            }
+        }
     }
 }
