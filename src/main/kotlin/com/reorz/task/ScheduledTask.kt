291d7e1f943a6a5cc4abe07179a76f8a383b7950
Acris Liu <acrisliu@gmail.com>
2016-08-14 13:43:59 +0800

    add scheduled task

---
+++ src/main/kotlin/com/reorz/task/ScheduledTask.kt
@@ -0,0 +1,21 @@
+package com.reorz.task
+
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.Clock
+
+/**
+ * Created by acris on 2016/8/11.
+ */
+@Component
+class ScheduledTask {
+    companion object {
+        val LOGTAG: String? = ScheduledTask::class.java.name
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    fun reportTimePerHour() {
+        val clock: Clock = Clock.systemDefaultZone()
+        print(clock.instant())
+    }
+}
