eb93bb419c53b4ae04551bbaed8c17064dd7ac2d
Acris Liu <acrisliu@gmail.com>
2016-08-29 19:58:28 +0800

    reformat code

--- src/main/kotlin/com/reorz/handler/MessageHandler.kt
+++ src/main/kotlin/com/reorz/handler/MessageHandler.kt
@@ -1,12 +1,15 @@
 package com.reorz.handler
 
+import com.reorz.turing.client.TuringClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.TelegramApiException
 import org.telegram.telegrambots.api.methods.send.SendMessage
+import org.telegram.telegrambots.api.objects.Message
 import org.telegram.telegrambots.api.objects.Update
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.logging.BotLogger
 
 /**
  * Created by acris on 2016/8/13.
@@ -14,6 +17,11 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot
 @Component
 class MessageHandler : TelegramLongPollingBot() {
     @Autowired lateinit var environment: Environment
+    @Autowired lateinit var turingClient: TuringClient
+
+    companion object {
+        val LOGTAG: String = MessageHandler::class.java.name
+    }
 
     override fun getBotUsername(): String {
         return environment.getProperty("app.bot.username")
@@ -23,19 +31,101 @@ class MessageHandler : TelegramLongPollingBot() {
         return environment.getProperty("app.bot.token")
     }
 
-    override fun onUpdateReceived(update: Update?) {
-        if (update!!.hasMessage()) {
-            val message = update.message
-            if (message.hasText()) {
-                val sm = SendMessage()
-                sm.chatId = message.chatId.toString()
-                sm.text = message.text
-                try {
-                    sendMessage(sm)
-                } catch (e: TelegramApiException) {
-
+    override fun onUpdateReceived(update: Update) {
+        when {
+            update.hasMessage() -> {
+                val message = update.message
+                when {
+                // user message
+                    message.isUserMessage -> handleUserMessage(message)
+                // group message
+                    message.isGroupMessage -> handleGroupMessage(message)
+                // super group message
+                    message.isSuperGroupMessage -> handleSuperGroupMessage(message)
+                // channel message
+                    message.isChannelMessage -> handleChannelMessage(message)
                 }
             }
+            update.hasEditedMessage() -> {
+            }
+            update.hasInlineQuery() -> {
+            }
+            update.hasCallbackQuery() -> {
+            }
+            update.hasChosenInlineQuery() -> {
+            }
+            else -> {
+                BotLogger.info(LOGTAG, "Update ${update.updateId} is empty.")
+            }
         }
+    }
+
+    /**
+     * User message handler
+     */
+    fun handleUserMessage(message: Message) {
+        when {
+            message.hasText() -> {
+                val text = message.text
+                when {
+                    // if message is command
+                    message.isCommand -> {
+                        when {
+                            text.startsWith("/start") -> {
+                            }
+                            text.startsWith("/help") -> {
+                            }
+                            else -> {
+                                BotLogger.info(LOGTAG, "Unknown command.")
+                            }
+                        }
+                    }
+                    else -> {
+                        val sm = SendMessage()
+                        sm.chatId = message.chatId.toString()
+
+                        val entities = message.entities
+                        when {
+                            // use turing api if message don't have entities
+                            entities.isEmpty() -> {
+                                val key = environment.getProperty("app.turing.key")
+                                val info = message.text
+                                val userid = botUsername
+                                val loc: String? = null
+                                val response = turingClient.chat(key, info, userid, loc)
+                                sm.text = response!!.text
+                            }
+                        }
+                        try {
+                            sendMessage(sm)
+                        } catch (e: TelegramApiException) {
+                            BotLogger.error(LOGTAG, "Send message error.")
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    /**
+     * Group message handler
+     */
+    fun handleGroupMessage(message: Message) {
+
+    }
+
+    /**
+     * Super group message handler
+     */
+    fun handleSuperGroupMessage(message: Message) {
+
+    }
+
+    /**
+     * Channel message handler
+     */
+    fun handleChannelMessage(message: Message) {
+
     }
 }
